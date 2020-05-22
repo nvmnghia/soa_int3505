3bf4f610833cd8287bee0fed0917c8c7fdc8072f
nvmnghia <nvmnghia@gmail.com>
2020-05-22 17:05:30 +0700

    Scaffolding for Spring demo

---
+++ assignments/spring_boot/demo/src/test/java/vn/edu/vnu/uet/nvmnghia/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.vnu.uet.nvmnghia.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
